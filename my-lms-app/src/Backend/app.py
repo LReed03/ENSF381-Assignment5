d35400e0137bec9119e6d23eeda96ac477e89ded
Chamer5465 <chasemack@shaw.ca>
2025-04-11 15:51:41 -0600

    Added methods

--- my-lms-app/src/Backend/app.py
+++ my-lms-app/src/Backend/app.py
@@ -38,7 +38,7 @@ students = [{
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/enroll/<student_id>')
+@app.route('/enroll/<student_id>', methods=['POST'])
 def enroll(student_id):
     data = request.get_json()
     for e in students:
@@ -49,7 +49,7 @@ def enroll(student_id):
             return jsonify({"results": "Successfully dropped class."})
             
 
-@app.route('/drop/<student_id>')
+@app.route('/drop/<student_id>', methods=['DELETE'])
 def drop(student_id):
     data = request.get_json()
     for e in students:
@@ -64,7 +64,6 @@ def courses():
     with open('data/courses.json', 'r') as f:
         data = {"results": json.load(f)}
         return jsonify(data)
-    
     
 @app.route('/login', methods=['POST'])
 def login():
